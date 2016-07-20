7be1d38558a0bbf6e0730d357f54a77441c5ac1c
LordRorek <jwchaley@aol.com>
2016-07-20 15:56:34 -0400

    ResumeBuilder: Clean up the biography code.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,40 +1,34 @@
-var bio = {
-  "name": "Andrew Powers",
-  "role": "Web Developer",
-  "contacts": {
-    "mobile": "410-202-6088",
-    "email": "jwchaley@aol.com",
-    "github": "LordRorek",
-    "location": "Deal Island, MD, USA",
-  },
-  "welcomeMessage": "Welcome Fellow Technophiles",
-  "skills": [
-    "Python", " HTML", " CSS", " Git", " GitHub"
-  ],
-  "bioPic": "images/Me.jpg",
-};
+/*Biography*/
+/*Biography Variable*/
+var bio = {};
+bio.name = "Andrew Powers";
+bio.role = "Web Developer";
+bio.contacts = {};
+  bio.contacts.mobile = "410-202-6088";
+  bio.contacts.email = "jwchaley@aol.com";
+  bio.contacts.github = "LordRorek";
+  bio.contacts._location = "Deal Island, MD, USA";
+bio.welcomeMessage = "Welcome Fellow Technophiles";
+bio.skills = [
+  "Python", " HTML", " CSS", " Git", " GitHub"
+];
+bio.bioPic = "images/Me.jpg";
+/*Biography Variable*/
 
 /*Name, Role and Pic Formatted*/
-var name = bio.name;
-var formattedName = HTMLheaderName.replace("%data%", name);
-var role = bio.role;
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-var Pic = bio.bioPic;
-var formattedBioPic = HTMLbioPic.replace("%data%", Pic);
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
 $("#header").prepend(formattedBioPic);
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 /*Name, Role and Pic Formatted*/
 
 /*Contacts Formatted*/
-var mobile = bio.contacts.mobile;
-var formattedMobile = HTMLmobile.replace("%data%", mobile);
-var email = bio.contacts.email;
-var formattedEmail = HTMLemail.replace("%data%", email);
-var github = bio.contacts.github;
-var formattedGithub = HTMLgithub.replace("%data%", github);
-var _location = bio.contacts.location;
-var formattedLocation = HTMLlocation.replace("%data%", _location);
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+var formattedLocation = HTMLlocation.replace("%data%", bio.contacts._location);
 $("#topContacts").append(formattedMobile);
 $("#topContacts").append(formattedEmail);
 $("#topContacts").append(formattedGithub);
@@ -42,11 +36,10 @@ $("#topContacts").append(formattedLocation);
 /*Contacts Formatted*/
 
 /*Skills and Message Formatted*/
-var skills = bio.skills;
-var formattedSkills = HTMLskills.replace("%data%", skills);
-var message = bio.welcomeMessage;
-var formattedMessage = HTMLwelcomeMsg.replace("%data%", message);
+var formattedSkills = HTMLskills.replace("%data%", bio.skills);
+var formattedMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
 $("#header").append(formattedMessage);
 $("#header").append(HTMLskillsStart);
 $("#skills").append(formattedSkills);
 /*Skills and Message Formatted*/
+/*Biography*/
